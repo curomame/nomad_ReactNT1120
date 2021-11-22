0a94c55d8a72f0f039bc698eca003aae93f8b543
HyangRim <curomame@naver.com>
2021-11-22 17:12:06 +0900

    reactnative weather app

--- App.js
+++ App.js
@@ -1,24 +1,88 @@
+import * as Location from "expo-location"
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { useState,useEffect } from "react";
+import { View, Text, StyleSheet, ScrollView, ActivityIndicator, Dimensions } from 'react-native';
+import { Fontisto } from '@expo/vector-icons'
+
+
+
+const {width:SCREEN_WIDTH} = Dimensions.get("window");
+// same to const SCREEN_WIDTH = Dimensions.get("window").width;
+
+const API_KEY = "32d86a6f1473247c8b4fd7aca2ab71a2"
+
+const icons = {
+  Clouds:"cloudy",
+  Clear:"day-sunny",
+  Atmosphere:"",
+  Snow:"snow",
+  Rains:"rains",
+  Drizzle:"rain",
+  Thunderstorm:"lightning",
+};
+
 
 export default function App() {
-  return (
+  const [city, setCity] = useState("Loading....");
+  const [days, setDays] = useState([]);
+  const [ok, setOk] = useState(true);
+  const getWeather = async() => {
+    const {granted} = await Location.requestForegroundPermissionsAsync();
     
+    if(!granted){
+      setOk(false);
+    }
+
+    
+    const {coords:{latitude, longitude}} = await Location.getCurrentPositionAsync({accuracy:5});
+
+    const location = await Location.reverseGeocodeAsync(
+      { latitude, longitude},
+      { useGoogleMaps : false}
+      );
+    setCity(location[0].city);
+      const response = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=alerts&appid=${API_KEY}&units=metric`)
+      const json = await response.json();
+  setDays(json.daily);
+  };
+
+  useEffect(() =>{
+  getWeather();
+},[]);
+
+
+
+  return (
+  
       <View style={styles.contaier}>
         <View style={styles.city}>
-          <Text style={styles.cityName}> BUSAN </Text>
+          <Text style={styles.cityName}> {city} </Text>
         </View>
 
-        <View style={styles.weather}>
-          <View style={styles.day}>
-            <Text style={styles.temp}>27</Text>
-            <Text style={styles.desc} >SUNNY</Text>
+        <ScrollView pagingEnabled horizontal showsHorizontalScrollIndicator={false} contentContainerStyle={styles.weather}>
+
+            {days.length === 0 ? 
+            <View style={styles.day}><ActivityIndicator  style={{...styles.day, alignItems:"center"}}/></View> : 
+            
+            (days.map((day,index) => 
+              <View key={index} style={styles.day}>
+                <View style={{flexDirection:"row", alignItems:"center", justifyContent:"space-between", width:"100%"}}>
+                <Fontisto name={icons[day.weather[0].main]} size={68} color="white" />
+                <Text style={styles.temp}>{parseFloat(day.temp.day).toFixed(1)}</Text>
+                </View>
+                <Text style={styles.desc}>{day.weather[0].main}</Text>
+                <Text style={styles.tinyText}>{day.weather[0].description}</Text>
+              </View>
+
+            ))
+            }
+
+
+            </ScrollView>
           </View>
-        </View>
-      </View>
-
   );
 }
+
 
 const styles = StyleSheet.create({
   contaier : {
@@ -26,32 +90,47 @@ const styles = StyleSheet.create({
     backgroundColor:"pink"
   },
   city : {
+    
     flex:1,
     justifyContent: "center",
-    alignItems:"center"
+    alignItems:"flex-start"
 },
 cityName : {
-  fontSize:68,
+  color:"white",
+  fontSize:50,
   fontWeight:"500"
 },
   weather : {
-    flex:3,
-    backgroundColor:"skyblue"
+  
   },
   day:{
-    flex:1,
-    alignItems:"center",
-    backgroundColor:"teal"
+    color:"white",
+    
+    marginTop:10,
+    width:SCREEN_WIDTH,
+    alignItems:"flex-end",
   },
   temp:{
+    color:"white",
     marginTop:20,
-    fontSize:188
+    fontSize:100,
+    marginRight:10
+
   },
   desc:{
-    marginTop:-30,
-    fontSize:25
+    color:"white",
+    marginTop:-20,
+    fontSize:40,
+    marginRight:10
+
+  },
+  tinyText:{
+    color:"white",
+    fontSize:20,
+    marginRight:10
   }
 })
+
 
 
 //좋은 자동완성기능을 제공
